4e263ea767106f96820b945b1ad16c6c49a1476e
Jose <josedev337@gmail.com>
2025-08-11 17:14:33 -0300

    molduras

---
+++ assets/js/modules/listas.js
@@ -0,0 +1,11 @@
+const molduras = {
+    verde: 'url(./assets/img/molduras/moldura-cartas-verde.png)', 
+    roxo: 'url(./assets/img/molduras/moldura-cartas-roxo.png)',
+    azulesverdiado: 'url(./assets/img/molduras/moldura-cartas-azul-esverdiado.png)',
+    azul: 'url(./assets/img/molduras/moldura-cartas-azul.png)',
+    vermelha: 'url(./assets/img/molduras/moldura-cartas-vermelha.png)'
+}
+
+export { molduras };
+
+// / {"andyrubin": "andy-rubin-pai-do-android"},
